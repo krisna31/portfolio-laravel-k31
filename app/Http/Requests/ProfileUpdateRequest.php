909d4977d136f492a4a56a89e58577c39b9275f7
Krisna31 <81416312+krisna31@users.noreply.github.com>
2023-02-12 12:40:13 +0700

    Update ProfileUpdateRequest.php

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -8,16 +8,6 @@ use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
-        /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return auth()->check();
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
